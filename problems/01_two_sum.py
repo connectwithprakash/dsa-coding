4160ba6815825c92451476f87c1875118e680088
Prakash Chaudhary <connectwithprakash@yahoo.com>
2021-01-12 12:26:47 +0545

    [Feat]: Improve algorithms of problem 1 and 2

--- problems/01_two_sum.py
+++ problems/01_two_sum.py
@@ -1,21 +1,29 @@
 class Solution:
+    
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        '''
+        
         count = 0
         temp = nums
-        while count<len(nums)-1:
+        total = len(nums)
+        while count<total-1:
             count += 1
             temp = temp[-1:]+temp[:-1]
             results = [sum(pair) for pair in zip(nums, temp)]
             if target in results:
-                y_idx = [index for index, num in enumerate(results) if num == target]
-                x_idx = [y_idx[0] - count]
+                for index, num in enumerate(results):
+                    if num == target:
+                        y_idx = index
+                        break
+                x_idx = (y_idx+total-count)%total
+                #x_idx = y_idx - count
                 break
-        return x_idx+y_idx
+        return [x_idx,y_idx]
         '''
         for i,f in enumerate(nums):
             temp = nums
-            temp[i]=1000
+            temp[i] = 10000
             for j,s in enumerate(temp):
                 if f+s == target:
                     return [i, j]
+        
+        '''
