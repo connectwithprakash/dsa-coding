e91b242636c10a83a646de6323d3b61645c4a60a
Prakash Chaudhary <connectwithprakash@yahoo.com>
2021-01-14 14:09:19 +0545

    [Update]: Change code to use round robin and solve for string of any length
    1. Solves for any length of string but time limit exceeds

--- problems/05_longest_palindromic_substring.py
+++ problems/05_longest_palindromic_substring.py
@@ -1,31 +1,21 @@
 class Solution:
     def rotate(self, string: str, rotate_n_char: int) -> str:
-        return string[rotate_n_char:] + string[:rotate_n_char]
+        string = string[rotate_n_char:] + string[:rotate_n_char]
+        return string
 
     def is_palindrome(self, s: str) -> bool:
-        print(s)
-        if len(s) == 0:
-            return False
-        else:
-            return s == ''.join(reversed(list(s)))
+        s1 = ''.join(s)
+        s2 = ''.join(reversed(s))
+        return s1 == s2
 
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        indices, palindromes, lengths = [], [], []
-
-        for i in range(0, n):
-            if self.is_palindrome(s[i-1:i+2]):
-                indices.append(i)
-
-        for i in indices:
-            j = 1
-            while 2*j+1 <= n:
-                word = s[i-j:i+j+1]
-                if self.is_palindrome(word):
-                    j += 1
-                else:
-                    break
-
-            palindromes.append(s[i-j+1:i+j])
-            lengths.append(2*j+1)
-        return palindromes[lengths.index(max(lengths))]
+        if s == ''.join(reversed(list(s))):
+            return s
+        for i in reversed(range(1, n)):
+            combinations = list(zip(*[self.rotate(s, j) for j in range(i)]))
+            index = n-i+1
+            combinations = combinations[:index]
+            has_palindromes = [self.is_palindrome(word) for word in combinations]
+            if True in has_palindromes:
+                return ''.join(combinations[has_palindromes.index(True)])
