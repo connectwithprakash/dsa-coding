d4fa47faabbcf73850b32e53cec3c55daf97f3fc
Prakash Chaudhary <connectwithprakash@yahoo.com>
2021-01-20 15:22:22 +0545

    [Feat]: Solving Longest Palindromic Substring using my DP imlementation.
    1. Solves the problem but time limit exceeds.

--- problems/05_longest_palindromic_substring.py
+++ problems/05_longest_palindromic_substring.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-
-
 class Solution:
     def rotate(self, string: str, rotate_n_char: int) -> str:
         string = string[rotate_n_char:] + string[:rotate_n_char]
@@ -9,87 +6,51 @@ class Solution:
     def find_even_odd_len_palindrome(self, word_length, combinations, palindrome_indices, palindrome_word, even=False):
         if even:
             start = 4
-            step = 1
         else:
             start = 5
-            step = 1
-        # print(palindrome_word)
-        print(palindrome_indices)
+        longest_palindrome_length = start - 2
         for palindrome_length in range(start, word_length + 1, 2):
             palindrome_new_indices = []
             for index in palindrome_indices:
-                new_index = index - step
-                print(index, new_index, word_length - step, word_length, word_length - palindrome_length)
-                if (new_index >= 0) and (new_index <= word_length - palindrome_length) and (
-                        index < word_length - step - 1):
-                    print('continue')
-                    pass
-                else:
-                    print('break')
-                    continue
-                word = combinations[new_index]
-                if word[0] == word[palindrome_length - 1]:
-                    palindrome_new_indices.append(new_index)
-            if len(palindrome_new_indices) > 0 and (palindrome_length <= word_length):
+                search_index = index - 1
+                if search_index >= 0:
+                    end_index = word_length-search_index
+                    word = combinations[search_index][:end_index]
+                    if (palindrome_length <= end_index) and (word[0] == word[palindrome_length-1]):
+                        palindrome_new_indices.append(search_index)
+
+            if len(palindrome_new_indices):
                 palindrome_indices = palindrome_new_indices
-                palindrome_word = combinations[palindrome_indices[0]][:palindrome_length]
+                longest_palindrome_length = palindrome_length
             else:
-                palindrome_word = combinations[palindrome_indices[0]][:palindrome_length - 2]
                 break
-        # palindrome_word = combinations[true_indices[0]][:palindrome_length]
-        return ''.join(palindrome_word)
+
+        return palindrome_indices[0], longest_palindrome_length
 
     def longestPalindrome(self, s: str) -> str:
         word_length = len(s)
         combinations = list(zip(*[self.rotate(s, i) for i in range(word_length)]))
 
         palindrome_indices = [index for index, word in enumerate(combinations[:word_length - 1]) if word[0] == word[1]]
-        # pprint(combinations[:word_length - 1][9])
-        # pprint(len(combinations))
         palindrome_new_indices = [index for index, word in enumerate(combinations[:word_length - 2]) if word[0] ==
                                   word[2]]
 
-        # if len(palindrome_new_indices) > 0:
-        #     palindrome_indices = palindrome_new_indices
-        #     palindrome_word = combinations[palindrome_indices[0]][:3]
-        #     step = 1
-        #     start = 5
-        # elif len(palindrome_indices) > 0:
-        #     palindrome_word = combinations[palindrome_indices[0]][:2]
-        #     step = 2
-        #     start = 4
-        # else:
-        #     palindrome_word = combinations[0][0]
-        #     start = word_length + 1
-        even_palindrome = odd_palindrome = ''
+        even_palindrome_len = odd_palindrome_len = 0
         if len(palindrome_indices) != 0:
             even_palindrome = combinations[palindrome_indices[0]][:2]
-            even_palindrome = self.find_even_odd_len_palindrome(word_length, combinations, palindrome_indices,
+            even_palindrome_index, even_palindrome_len = self.find_even_odd_len_palindrome(word_length, combinations,
+                                                                           palindrome_indices,
                                                                 even_palindrome, True)
         if len(palindrome_new_indices) != 0:
             odd_palindrome = combinations[palindrome_new_indices[0]][:3]
-            odd_palindrome = self.find_even_odd_len_palindrome(word_length, combinations, palindrome_new_indices,
+            odd_palindrome_index, odd_palindrome_len = self.find_even_odd_len_palindrome(word_length, combinations,
+                                                                                        palindrome_new_indices,
                                                                odd_palindrome, False)
-
-        if len(even_palindrome) > len(odd_palindrome):
-            palindrome_word = even_palindrome
-        elif len(odd_palindrome):
-            palindrome_word = odd_palindrome
+        if even_palindrome_len > odd_palindrome_len:
+            palindrome_word = combinations[even_palindrome_index][:even_palindrome_len]
+        elif odd_palindrome_len:
+            palindrome_word = combinations[odd_palindrome_index][:odd_palindrome_len]
         else:
             palindrome_word = combinations[0][0]
 
-        # for palindrome_length in range(start, word_length + 1, 2):
-        #     palindrome_new_indices = []
-        #     for index in palindrome_indices:
-        #         word = combinations[index - step]
-        #         if word[0] == word[palindrome_length-1]:
-        #             palindrome_new_indices.append(index - step)
-        #     if len(palindrome_new_indices) > 0 and (palindrome_length <= word_length):
-        #         palindrome_indices = palindrome_new_indices
-        #         palindrome_word = combinations[palindrome_indices[0]][:palindrome_length]
-        #     else:
-        #         palindrome_word = combinations[palindrome_indices[0]][:palindrome_length - 2]
-        #         break
-        # # palindrome_word = combinations[true_indices[0]][:palindrome_length]
-        # return ''.join(palindrome_word)
-        return palindrome_word
+        return ''.join(palindrome_word)
