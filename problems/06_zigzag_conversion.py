953bef7a801e733bc5e4ecf5394a11010dcf61b9
Prakash Chaudhary <connectwithprakash@yahoo.com>
2021-01-21 15:03:11 +0545

    [Feat]: Solve ZigZag Conversion for general case
    1. Doesn't work for single char word and other degenerate cases, needs improvement.

--- problems/06_zigzag_conversion.py
+++ problems/06_zigzag_conversion.py
@@ -1,3 +1,50 @@
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        pass
+    def convert(self, string: str, numRows: int) -> str:
+        word_length = len(string)
+        n_rows = numRows
+
+        total_all_filled_cols = 1 + (word_length - 1) // (2 * n_rows - 2)
+        total_single_filled_cols = total_all_filled_cols * (n_rows - 2)
+        total_cols = total_all_filled_cols + total_single_filled_cols
+
+        start = n_rows + 1
+        stop = start + (n_rows - 1) * (total_all_filled_cols - 1) + 1
+        step = n_rows - 1
+        zigzag_values = list(reversed(range(start, stop, step)))
+
+        table = [['' for j in range(total_cols)] for i in range(n_rows)]
+
+        string = list(reversed(string))
+        value = 0
+        for j in range(total_cols):
+            count = (j) % (n_rows-1)
+            if count == 0:
+                value = zigzag_values.pop()
+
+            for i in range(n_rows):
+                print(i+1,j+1)
+                print(value)
+                print(string)
+                try:
+                    if count > 0:
+                        print('single')
+                        if (i + j + 2) == value:
+                            letter = string.pop()
+                            table[i][j] = letter
+                    else:
+                        print('full')
+                        letter = string.pop()
+                        print(letter)
+                        table[i][j] = letter
+                except:
+                    table[i][j] = ''
+            count += 1
+
+        print(table[0])
+        print(table[1])
+        print(table[2])
+        zigzag_word = ''
+        for row in table:
+            zigzag_word += ''.join(row)
+
+        return zigzag_word
