1af1bdc3a90f45813d3e4f87ce4112a861bc028d
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-01-30 07:51:26 -0600

    [Feat]: Add solution to "Is Subsequence" problem.

---
+++ problems/392_is_subsequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        j = 0
+        s_len = len(s)
+        for i in range(len(t)):
+            if (j < s_len):
+                if (s[j] == t[i]):
+                    j += 1
+        return (j == s_len)
