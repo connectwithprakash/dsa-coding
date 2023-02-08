3071d525e689c7de91d15807bc711a28bf73f5b3
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-07 22:57:20 -0600

    [Fix]: Update solution for Binary Tree Level Order Traversal.
    Use start and end indices to find the elements at a level from frontier. Reduces space and time complexity.

--- problems/102_binary_tree_level_order_traversal.py
+++ problems/102_binary_tree_level_order_traversal.py
@@ -7,28 +7,23 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if root is None:
-            return []
-        else:
-            # Here 1 denotes the start while 2 denotes the stop of the level nodes in the frontier
-            frontier = [1, root, 2]
             level_order_traversal = []
-            index = 0
-            while index != len(frontier):
-                if (frontier[index] == 1):
-                    if (frontier[index+1] != 2):
-                        level_items = []
-                        frontier.append(1)
-                    else:
-                        break
-                elif (frontier[index] == 2):
-                    level_order_traversal.append(level_items)
-                    frontier.append(2)
-                else:
-                    level_items.append(frontier[index].val)
-                    if frontier[index].left is not None:
-                        frontier.append(frontier[index].left)
-                    if frontier[index].right is not None:
-                        frontier.append(frontier[index].right)
-                index += 1
-            return level_order_traversal
+        else:
+            frontier = [root]
+            start = 0
+            end = len(frontier)
+            level_order_traversal = []
+            while start != end:
+                level_items = []
+                for node in frontier[start:end]:
+                    level_items.append(node.val)
+                    if node.left is not None:
+                        frontier.append(node.left)
+                    if node.right is not None:
+                        frontier.append(node.right)
+                level_order_traversal.append(level_items)
+                start = end
+                end = len(frontier)
+
+        return level_order_traversal
 
