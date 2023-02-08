5732e2ff8f5500826c8f7e87868cd0f999460220
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-07 23:13:28 -0600

    [Docs]: Add approach used to the bianry tree level oprder traversal problem.

--- problems/102_binary_tree_level_order_traversal.py
+++ problems/102_binary_tree_level_order_traversal.py
@@ -1,3 +1,20 @@
+"""
+# Intuition
+The idea is to create a frontier that acts as a queue for BFS. From the frontier some how extract the elements that are on the same level. For that we try to find where the first node at a level is and the last node is denoted by start and end indices. We then use these indices to extract the node value iterating through these indices.
+
+# Approach
+We first instantiate frontier with the root node, start with 0 and end with length of the frontier.
+1. Check if the start and end indices are same.
+2. Loop through the elements given by start and end-1 indices and add them to level nodes list and look for its children and add them to frontier.
+3. Update the start index with end index and end index with length of updated frontier.
+4. Repeat step 1 to 3.
+
+# Complexity
+- Time complexity: $$O(n)$$
+
+- Space complexity: $$O(n)$$
+"""
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
