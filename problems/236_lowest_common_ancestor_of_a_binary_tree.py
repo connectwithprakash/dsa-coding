a725cf5e1a133e6b8f36ef42db1ecdca88b97628
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-10 11:42:42 -0600

    [Fix]: Add a new efficient algorithm to solve lowest common ancestor in binary tree.

--- problems/236_lowest_common_ancestor_of_a_binary_tree.py
+++ problems/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -6,31 +6,19 @@
 #         self.right = None
 
 class Solution:
-    def find_path(self, root: 'TreeNode', node: 'TreeNode') -> List['TreeNode']:
-        path = []
-        if root is node:
-            path.append(root)
-        else:
-            if root.left:
-                left = self.find_path(root.left, node)
-                path += left
-            if root.right:
-                if node not in path:
-                    right = self.find_path(root.right, node)
-                    path += right
-            if node in path:
-                    path.append(root)
-        return path
-    
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        p_paths = self.find_path(root, p)
-        q_paths = self.find_path(root, q)
-        if len(p_paths) > len(q_paths):
-            for node in p_paths:
-                if node in q_paths:
-                    return node
-        else:
-            for node in q_paths:
-                if node in p_paths:
-                    return node
+        if root is None:
+            return None
+        if (root is p) or (root is q):
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if (left is not None) & (right is not None):
+            return root
+        elif left is not None:
+            return left
+        elif right is not None:
+            return right
 
