b787a256971a2fd94262e28f0d1021b09f0e8011
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-10 07:36:12 -0600

    [Fix]: Add different solution (not better than previous) to Common Ancestor problem.
    The solution uses traversal through the tree for p and q each and then we find the lowest common node in their paths.

--- problems/235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ problems/235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -6,58 +6,31 @@
 #         self.right = None
 
 class Solution:
-    def find_smaller_node(self, root: 'TreeNode', node: 'TreeNode') -> List['TreeNode']:
-        parents = []
+    def find_path(self, root: 'TreeNode', node: 'TreeNode') -> List['TreeNode']:
+        path = []
         if root is node:
-            parents.append(root)
+            path.append(root)
         else:
             if root.left:
-                left = self.find_smaller_node(root.left, node)
-                parents += left
+                left = self.find_path(root.left, node)
+                path += left
             if root.right:
-                if node not in parents:
-                    right = self.find_smaller_node(root.right, node)
-                    parents += right
-            if node in parents:
-                    parents.append(root)
-        return parents
-
-
-    def find_bigger_node(self, root: 'TreeNode', node: 'TreeNode', parents: List['TreeNode']) -> 'TreeNode':
-        node_found = False
-        common_ancestor = None
-        if (root is node):
-            common_ancestor = root
-            node_found = True
-        else:
-            if root.left:
-                node_found, left = self.find_bigger_node(root.left, node, parents)
-                if node_found:
-                    if left in parents:
-                        common_ancestor = left
-                    elif root in parents:
-                        common_ancestor = root
-            if (root.right is not None) & (not node_found):
-                if not node_found:
-                    node_found, right = self.find_bigger_node(root.right, node, parents)
-                if node_found & (common_ancestor is None):
-                    if right in parents:
-                        common_ancestor = right
-                    elif root in parents:
-                        common_ancestor = root
-
-        return node_found, common_ancestor
-                
+                if node not in path:
+                    right = self.find_path(root.right, node)
+                    path += right
+            if node in path:
+                    path.append(root)
+        return path
     
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if p.val < q.val:
-            smaller_node = p
-            bigger_node = q
+        p_paths = self.find_path(root, p)
+        q_paths = self.find_path(root, q)
+        if len(p_paths) > len(q_paths):
+            for node in p_paths:
+                if node in q_paths:
+                    return node
         else:
-            smaller_node = q
-            bigger_node = p
-
-        parents_of_smaller_node = self.find_smaller_node(root, smaller_node)
-        _, lowest_common_ancestor = self.find_bigger_node(root, bigger_node, parents_of_smaller_node)
-        return lowest_common_ancestor
+            for node in q_paths:
+                if node in p_paths:
+                    return node
 
