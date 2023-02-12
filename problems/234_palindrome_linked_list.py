ba749aa92d0a799286ceeebb021da504925a5065
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-12 13:49:49 -0600

    [Refactor]: Add comments.

--- problems/234_palindrome_linked_list.py
+++ problems/234_palindrome_linked_list.py
@@ -3,24 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         reverse = None
         slow = fast = head
-
+        # Reverse the first half till the median is found
         while (fast is not None) and (fast.next is not None):
             fast = fast.next.next
-
+            # Reversing the first half
             temp = slow.next
             slow.next = reverse
             reverse = slow
             slow = temp
-
+        # Instantiating forward pointer with appropriate start position
         if fast is not None:
             forward = slow.next
         else:
             forward = slow
-
+        # Looping through halves using two pointers
         while forward is not None:
             if forward.val == reverse.val:
                 forward = forward.next
