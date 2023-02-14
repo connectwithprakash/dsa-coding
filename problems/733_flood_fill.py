49230671d282652c71d6d84101e3afd24b1d55a1
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-14 10:15:52 -0600

    [Feat]: Add recursive solution to "Flood Fill" problem.

--- problems/733_flood_fill.py
+++ problems/733_flood_fill.py
@@ -1,3 +1,29 @@
+"""
+# Recursive solution
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m, n = len(image)-1, len(image[0])-1
+        src_color = image[sr][sc]
+        
+        def fill_neighbors(x, y):
+            if (image[x][y] != src_color) or (image[x][y] == color):
+                return
+            else:
+                image[x][y] = color
+            
+            for dx, dy in [(-1, 0), (0, -1), (1, 0), (0,1)]:
+                x_new = x+dx
+                y_new = y+dy
+                if (0 <= x_new <= m) and (0 <= y_new <= n):
+                    fill_neighbors(x_new, y_new)
+        
+        fill_neighbors(sr, sc)
+
+        return image
+"""
+
+# Iterative Solution with Space complexity of O(m+n)
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         indices = [(sr, sc)]
