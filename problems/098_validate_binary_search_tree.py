888d68126f3faf2ef9cdfb9da2d90a6eb0a8aae1
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-16 15:48:22 -0600

    [Chore]: Add efficient and intuitive algorithm to solve Validate Binary Search Tree.

--- problems/098_validate_binary_search_tree.py
+++ problems/098_validate_binary_search_tree.py
@@ -5,28 +5,18 @@
 #         self.left = left
 #         self.right = right
 
-
 class Solution:
-    def inOrderTraversal(self, root, prev_val, prev_is_valid):
-        if not prev_is_valid:
-            return prev_val, prev_is_valid
-        if root.left:
-            prev_val, left_is_valid = self.inOrderTraversal(root.left, prev_val, prev_is_valid)
-            prev_is_valid = prev_is_valid & left_is_valid
-        if root.val > prev_val:
-            prev_val = root.val
-            prev_is_valid = (prev_is_valid & True)
+    def inOrderTraversal(self, node, low_limit, high_limit):
+        if node is None:
+            return True
+        elif (low_limit < node.val < high_limit):
+            left_is_valid = self.inOrderTraversal(node.left, low_limit, node.val)
+            right_is_valid = self.inOrderTraversal(node.right, node.val, high_limit)
+            
+            return left_is_valid and right_is_valid
         else:
-            prev_is_valid = (prev_is_valid & False)
-        if root.right:
-            prev_val, right_is_valid = self.inOrderTraversal(root.right, prev_val, prev_is_valid)
-            prev_is_valid = prev_is_valid & right_is_valid
-
-        return prev_val, prev_is_valid
+            return False
 
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        prev_val = -2e31
-        prev_val, is_valid_vst = self.inOrderTraversal(root, prev_val, True)
-        
-        return is_valid_vst
+        return self.inOrderTraversal(root, -float("inf"), float("inf"))
 
