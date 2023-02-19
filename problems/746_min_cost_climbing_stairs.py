03363af4b3e3a94eccb769406d315da081567c7b
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-19 10:22:29 -0600

    [Feat]: Add solution with memoization for "Climbing Stairs".

--- problems/746_min_cost_climbing_stairs.py
+++ problems/746_min_cost_climbing_stairs.py
@@ -1,9 +1,14 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = {}
         def helper(n):
             if n < 2:
                 return 0
-            else:
-                return min(cost[n-1]+helper(n-1), cost[n-2]+helper(n-2))
+            if (n-1) not in dp:
+                dp[n-1] = helper(n-1)
+            if (n-2) not in dp:
+                dp[n-2] = helper(n-2)
+
+            return min(cost[n-1]+dp[n-1], cost[n-2]+dp[n-2])
         return helper(len(cost))
 
