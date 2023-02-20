05d86b116f3f940d9a1ea05656fb6f90c00568cf
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-20 14:21:45 -0600

    [Refactor]: Decrease space complexity from O((m+1)*(n+1)) to  O(mxn) for "Unique Paths" problem.

--- problems/062_unique_paths.py
+++ problems/062_unique_paths.py
@@ -18,13 +18,19 @@ class Solution:
 """
 
 # DP - memoization solution
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0 for j in range(n+1)] for i in range(m+1)]
-        dp[0][1] = 1
-        for i in range(1, m+1):
-            for j in range(1, n+1):
+        dp = [[0]*n for i in range(m)]
+        
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         
-        return dp[m][n] 
+        return dp[m-1][n-1] 
 
