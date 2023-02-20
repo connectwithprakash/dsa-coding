a6ef3bd8faec7f2e119abf7028746200192193bd
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-20 14:16:51 -0600

    [Feat]: Add DP solution for "Unique Paths".
    This solved the maximum time limit exceeded issue with recursive solution.

--- problems/062_unique_paths.py
+++ problems/062_unique_paths.py
@@ -1,3 +1,6 @@
+"""
+# Recursive solution
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         def dfs(x, y):
@@ -12,4 +15,16 @@ class Solution:
                 return (right + left)
 
         return dfs(0, 0)
+"""
+
+# DP - memoization solution
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0 for j in range(n+1)] for i in range(m+1)]
+        dp[0][1] = 1
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[m][n] 
 
