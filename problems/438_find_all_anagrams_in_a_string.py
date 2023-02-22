bba394a947667b78d51ed911b7315280b7a5bdf6
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-22 16:19:56 -0600

    [Feat]: Add faster (beats 99%) solution for "Find All Anagrams in a String".
    Beats 99% of the submission.

--- problems/438_find_all_anagrams_in_a_string.py
+++ problems/438_find_all_anagrams_in_a_string.py
@@ -1,3 +1,4 @@
+"""
 # Brute-force naive O(n^2) solution
 
 class Solution:
@@ -20,5 +21,46 @@ class Solution:
                     break
             if found:
                 indices.append(i)
+        return indices
+"""
+
+# Sliding window using two pointers
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        indices = []
+        p_map = {}
+        for c in p:
+            p_map[c] = 0
+        for c in p:
+            p_map[c] += 1
+        temp_p_map = p_map.copy()
+        len_p = len(p)
+        count = 0
+        for i in range(len(s)):
+            if not count:
+                start = i
+            if temp_p_map.get(s[i], 0):
+                count += 1
+                temp_p_map[s[i]] -= 1
+            else:
+                if s[i] in temp_p_map:
+                    for j in range(start, i):
+                        temp_p_map[s[j]] += 1
+                        count -= 1
+                        if s[j] == s[i]:
+                            temp_p_map[s[j]] -= 1
+                            count += 1
+                            break
+                    start = j+1
+                else:
+                    temp_p_map = p_map.copy()
+                    count = 0
+            if count == len_p:
+                temp_p_map[s[start]] += 1
+                indices.append(start)
+                start += 1
+                count -= 1
+
         return indices
 
