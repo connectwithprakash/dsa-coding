22ef0c49a4b69859ffc83939ab79200f525c28c8
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-02-24 17:26:11 -0600

    [Feat]: Add O(1) space complexity solution to "Backspace String Compare".

--- problems/844_backspace_string_compare.py
+++ problems/844_backspace_string_compare.py
@@ -1,3 +1,5 @@
+"""
+# O(n) time and space complexity solution
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
         def get_text(text: str) -> str:
@@ -15,4 +17,36 @@ class Solution:
         t = get_text(t)
 
         return (s==t)
+
+"""
+
+# O(n) time complexity and O(1) space complexity
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def get_next_index(string, index):
+            count = 0
+            while (index > -1):
+                if string[index] != '#':
+                    if (count > 0):
+                        index -= 1
+                        count -= 1
+                    else:
+                        break
+                else:
+                    count += 1
+                    index -= 1
+
+            return index
+        
+        i, j = len(s)-1, len(t)-1
+        while True:
+            i = get_next_index(s, i)
+            j = get_next_index(t, j)
+            if ((i > -1) and (j >= -1)) and (s[i] == t[j]):
+                i -= 1
+                j -= 1
+            else:
+                break
+        return ((i == -1) and (j == -1))
 
