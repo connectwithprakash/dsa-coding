0870e07a781854c2128a2d0032c867c05db79ae7
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-03-28 10:40:56 -0500

    [Feat]: Add solution using heap for Top K Frequent Words p[roblem.

--- problems/692_top_k_frequent_words.py
+++ problems/692_top_k_frequent_words.py
@@ -10,3 +10,17 @@ class Solution:
         count = [k for k, v in sorted(count.items(), key=lambda item: item[1], reverse=True)]
         return count[:k]
 
+
+# Second Attempt: Using heap
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        count = {}
+        # Get counts of each word and store in a hash
+        for word in words:
+            count[word] = count.get(word, 0) + 1
+        # Create a order for sorting
+        count = [(-v, k) for k, v in count.items()]
+        heapq.heapify(count)
+        
+        return [heapq.heappop(count)[1] for _ in range(k)]
+
