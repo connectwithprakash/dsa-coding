a6decfd2f83c39435991a17382141375c6649b38
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-03-28 09:04:40 -0500

    [Feat]: Add O(n) space and time complexity using heap.

--- problems/1046_last_stone_weight.py
+++ problems/1046_last_stone_weight.py
@@ -1,3 +1,4 @@
+# Attempt 1: Bruteforce solution
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         while len(stones) > 1:
@@ -5,6 +6,26 @@ class Solution:
             diff = stones[-1] - stones[-2]
             stones = stones[:-2]
             stones.append(diff)
-            
+
         return stones[0]
+
+
+# Attempt 2: Using heap
+# O(n) space and time complexity solution
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        heap = []
+        for stone in stones:
+            heapq.heappush(heap, -stone)
+
+        while len(heap) > 1:
+            diff = heapq.heappop(heap) - heapq.heappop(heap)
+            if (diff < 0):
+                heapq.heappush(heap, diff)
+
+        if len(heap):
+            return -heap[0]
+        else:
+            return 0
+
 
