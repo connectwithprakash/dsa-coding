52df22b4d62e2e371276850cb2594186acf51369
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-03-31 20:16:03 -0500

    [Feat]: Add faster linear time solution to "Reverse String" problem.

--- problems/344_reverse_string.py
+++ problems/344_reverse_string.py
@@ -19,4 +19,16 @@ class Solution:
         recursive(p, q)
 
         return s
-  
+
+
+# Faster solution
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        for i in range(n//2):
+            s[i], s[n-i-1] = s[n-i-1], s[i]
+        
+        return s  
