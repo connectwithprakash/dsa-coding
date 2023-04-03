e03c574bd630e6c74d4ef8df3bb4c3c97112c67f
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-04-03 09:14:02 -0500

    [Feat]: Add recursive solution to "Search in a BST" problem.

---
+++ problems/700_search_in_a_binary_search_tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# First Attempt: Brute-force
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if (root.val == val):
+            return root
+        else:
+            sub_tree = None
+            if root.left:
+                sub_tree = self.searchBST(root.left, val)
+            if (sub_tree is None) and (root.right):
+                sub_tree = self.searchBST(root.right, val)
+        
+            return sub_tree
+
