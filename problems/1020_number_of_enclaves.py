1f468327e92c9551cf31024d0736cbaec0dc7682
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-04-10 18:11:07 -0500

    [Chore]: Add complete efficient solution to "Number of Enclaves" problem.

--- problems/1020_number_of_enclaves.py
+++ problems/1020_number_of_enclaves.py
@@ -1,23 +1,17 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        def traverse_island(x, y, has_boundary):
-            grid[x][y] = 2
-            if (x == 0) or (x == (m-1)) or (y == 0) or (y == (n-1)):
-                has_boundary = True 
+        def traverse_island(x, y):
+            grid[x][y] = 0
             for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
                 x_new, y_new = x+dx, y+dy
                 if (0 <= x_new < m) and (0 <= y_new < n) and (grid[x_new][y_new] == 1):
-                    has_boundary = traverse_island(x_new, y_new, has_boundary)
-            grid[x][y] = 1 * (not has_boundary)
-            return has_boundary
+                    traverse_island(x_new, y_new)
 
-        count = 0
         m, n = len(grid), len(grid[0])
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    traverse_island(i, j, False)
-                if grid[i][j] == 1:
-                    count += 1
-        return count
+        for x in range(m):
+            for y in range(n):
+                if (grid[x][y] == 1) and ((x == 0) or (x == (m-1)) or (y == 0) or (y == (n-1))):
+                    traverse_island(x, y)
+        
+        return sum([sum(row) for row in grid])
 
