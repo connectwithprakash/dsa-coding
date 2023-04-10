65f521c2fb6162b19db202ef03befa91dd124862
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-04-10 18:01:46 -0500

    [Feat]: Improve the solution to "Number of Enclaves" problem.

--- problems/1020_number_of_enclaves.py
+++ problems/1020_number_of_enclaves.py
@@ -1,13 +1,15 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         def traverse_island(x, y, has_boundary):
+            grid[x][y] = 2
             if (x == 0) or (x == (m-1)) or (y == 0) or (y == (n-1)):
-                has_boundary = True
-            grid[x][y] = 2 * (not has_boundary) 
+                has_boundary = True 
             for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
                 x_new, y_new = x+dx, y+dy
                 if (0 <= x_new < m) and (0 <= y_new < n) and (grid[x_new][y_new] == 1):
-                    traverse_island(x_new, y_new, has_boundary)
+                    has_boundary = traverse_island(x_new, y_new, has_boundary)
+            grid[x][y] = 1 * (not has_boundary)
+            return has_boundary
 
         count = 0
         m, n = len(grid), len(grid[0])
@@ -15,7 +17,7 @@ class Solution:
             for j in range(n):
                 if grid[i][j] == 1:
                     traverse_island(i, j, False)
-                if grid[i][j] == 2:
+                if grid[i][j] == 1:
                     count += 1
         return count
 
