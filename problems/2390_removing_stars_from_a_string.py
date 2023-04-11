a0b7bb278e6464ffd1cf600745aebdf35e494f08
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-04-11 13:40:05 -0500

    [Feat]: Add solution to "Removing Stars From a String" problem.

---
+++ problems/2390_removing_stars_from_a_string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        j = len(s) - 1
+        output = []
+        count = 0
+        while (j >= 0):
+            if (s[j] == "*"):
+                while (s[j] == "*"):
+                    count += 1
+                    j -= 1
+                while count:
+                    if (s[j] == "*"):
+                        break
+                    j -= 1
+                    count -= 1
+            else:
+                output.append(s[j])
+                j -= 1
+        return "".join(output[::-1])
+
