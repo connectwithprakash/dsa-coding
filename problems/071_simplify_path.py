fa708f24cb6e8ab98da4c1db5fbca370b35c6534
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-04-12 09:50:27 -0500

    [Refactor]: Make the code cleaner and efficient for "Simplify Path".

--- problems/071_simplify_path.py
+++ problems/071_simplify_path.py
@@ -1,20 +1,14 @@
-# Attempt 1: Brute force
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        path_split = path.split("/")
+        path = path.split("/")
         stack = []
-        for i in range(len(path_split)):
-            if path_split[i] == "..":
+        for path_split in path:
+            if (path_split == "") or (path_split == "."):
+                continue
+            elif (path_split == ".."):
                 if len(stack):
                     stack.pop()
-            elif (path_split[i] == "") or (path_split[i] == "."):
-                continue
             else:
-                stack.append("/"+path_split[i])
-        # stack = ["/"] + stack
-        if len(stack):
-            print(stack)
-            return "".join(stack)
-        else:
-            return "/"
- 
+                stack.append(path_split)
+        return "/" + "/".join(stack)
+
