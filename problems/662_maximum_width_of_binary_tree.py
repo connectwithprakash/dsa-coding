098ce9b239be07f6a60deef1c8a8506d3e93843c
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-04-20 16:08:54 -0500

    [Feat]: Add BFS for level order traversal solution to "Maximum Width of Binary Tree".

--- problems/662_maximum_width_of_binary_tree.py
+++ problems/662_maximum_width_of_binary_tree.py
@@ -7,35 +7,23 @@
 
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.left_widths = []
-        self.right_widths = []
-        def helper(node, dir, width):
-            if node is None:
-                return
-            if dir == "left":
-                self.left_widths.append(width)
+        queue = [(root, 0)]
+        level_queue = queue
+        max_width = 1
+        while queue:
+            level_queue = []
+            for node, i in queue:
                 if node.left:
-                    helper(node.left, dir, width*2)
-                else:
-                    helper(node.right, dir, (width*2-1))
-            if dir == "right":
-                self.right_widths.append(width)
+                    level_queue.append((node.left, 2*i+1))
                 if node.right:
-                    helper(node.right, dir, width*2)
-                else:
-                    helper(node.left, dir, (width*2-1))
+                    level_queue.append((node.right, 2*i+2))
+            
+            if level_queue:
+                level_width = level_queue[-1][1] - level_queue[0][1] + 1
+                if level_width > max_width:
+                    max_width = level_width
 
-        helper(root, "left", 1)
-        helper(root, "right", 1)
+            queue = level_queue
 
-        last_common_level = min(len(self.left_widths), len(self.right_widths))
-
-        max_width = 0
-        for i in range(last_common_level):
-            minus = (2**(i) - self.left_widths[i]) + (2**(i) - self.right_widths[i])
-            width = (2**(i)) - minus
-            if width > max_width:
-                max_width = width
-        
         return max_width
 
