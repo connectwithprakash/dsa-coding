55ee9d22a8a12ffdaa188615b6c2556f5c4c747c
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-06-06 09:09:24 -0500

    [Feat]: Add a complete O(n) solution to 'Merge Sorted Array'.

--- problems/088_merge_sorted_array.py
+++ problems/088_merge_sorted_array.py
@@ -4,19 +4,16 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         result = []
-        i, j = 0, 0
-        while (i < m) and (j < n):
-            if nums1[i] < nums2[j]:
-                result.append(nums1[i])
-                i += 1
+        i, j, k = m-1, n-1, (m+n-1)
+        while (i >= 0) and (j >= 0):
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                result.append(nums2[j])
-                j += 1
-
-        if (i < m):
-            result.extend(nums1[i:])
-        elif (j < n):
-            result.extend(nums2[j:])
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
         
-        return result
+        if (j >= 0):
+            nums1[:j+1] = nums2[:j+1]
 
