d581b126e9cd0ebe9371eef4947ff5425fe24c74
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-06-08 19:03:27 -0500

    [Docs]: Add notes on approach for 'Majority Element'.

--- problems/169_majority_element.py
+++ problems/169_majority_element.py
@@ -1,4 +1,11 @@
 # O(n) space and time complexity.
+
+# The idea is to find the element that is more frequent progressively.
+# A thing to remember is that the code scans element and increases the count
+# and decreases if the character is not same and we just want the count to be
+# greater than 0. Having count 0 means that no element is occurs more than n/2
+
+
 class Solution:
   def majorityElement(self, nums: List[int]) -> int:
     mode_val: int
