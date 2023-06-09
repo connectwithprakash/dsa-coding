29d2b4340eb5256e0364ffa0ee995c9a5e8a616a
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-06-08 19:02:43 -0500

    [Feat]: Add linear time and space complexity solution to 'Majority Element'.

--- problems/169_majority_element.py
+++ problems/169_majority_element.py
@@ -1,13 +1,14 @@
+# O(n) space and time complexity.
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        counts = {num: 0 for num in nums}
-        max_freq_val = nums[0]
-        max_freq = 1
-        for num in nums:
-            counts[num] += 1
-            if counts[num] > max_freq:
-                max_freq = counts[num]
-                max_freq_val = num
-
-        return max_freq_val
-
+  def majorityElement(self, nums: List[int]) -> int:
+    mode_val: int
+    freq = 0
+    for num in nums:
+        if (freq == 0):
+            mode_val = num
+        if (mode_val == num):
+            freq += 1
+        else:
+            freq -= 1
+    return mode_val
+ 
