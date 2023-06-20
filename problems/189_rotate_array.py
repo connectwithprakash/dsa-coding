c123dc57d6e3cd5105d3114282cac42541f636e7
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-06-19 20:54:17 -0500

    [Feat]: Add my own O(n) time and O(1) space complexity solution for 'Rotate Array'.

--- problems/189_rotate_array.py
+++ problems/189_rotate_array.py
@@ -1,3 +1,4 @@
+## Attempt 1: Pythonic solution
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
@@ -6,4 +7,27 @@ class Solution:
         # Naive pythonic way
         k = (k % len(nums))
         nums[:] = nums[-k:] + nums[:-k]
+
+
+## Approach 2: Divide the array into from pivot idx and reverse each and then reverse combined.
+
+
+## Attempt 2: My solution
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        offset = 0
+        rem_n = len(nums)
+        n = len(nums)
+        while True:
+            k = k % rem_n
+            if k == 0:
+                break
+            for i in range(n-k, n):
+                new_i = ((i+k)%n)+offset
+                nums[new_i], nums[i] = nums[i], nums[new_i]
+            rem_n = rem_n-k
+            offset += k
 
