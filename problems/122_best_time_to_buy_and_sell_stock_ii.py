6b3d3d3601f5fbbbcdb17c074d1bafe9f73f6c5d
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-06-20 21:14:26 -0500

    [Feat]: Add solution to "Best Time to Buy and Sell Stock II".

---
+++ problems/122_best_time_to_buy_and_sell_stock_ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        prev_stock = prices[0]
+        total_stock = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prev_stock:
+                total_stock += prices[i] - prev_stock
+                prev_stock = prices[i]
+            else:
+                prev_stock = prices[i]
+
+        return total_stock
+
