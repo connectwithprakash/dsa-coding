fb3e6acccc7da9a1db6dd1c54934276d6f2006ff
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-06-22 15:04:53 -0500

    Feat]: Add O(n) time and space complexity with one less array solution for "Product of Array Except Self".

--- problems/238_product_of_array_except_self.py
+++ problems/238_product_of_array_except_self.py
@@ -1,3 +1,4 @@
+## Attempt 1: Using two additional O(n) list
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
@@ -12,4 +13,21 @@ class Solution:
             nums[i] = left_prod[i] * right_prod[i]
 
         return nums
+
+
+## Attempt 2: Using single additional O(n) list
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)   
+        products = [1]*n
+
+        for i in range(1, n):
+            products[i] = products[i-1] * nums[i-1]
+        
+        right_prod = 1
+        for i in range(n-2, -1, -1):
+            right_prod *= nums[i+1]
+            products[i] = products[i] * right_prod
+    
+        return products
 
