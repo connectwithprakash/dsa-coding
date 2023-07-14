a66fb50ebe444738af388e399313d09e93b56179
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-07-14 17:37:44 -0500

    [Feat]: Add O(1) solution to "Insert Delete GetRandom O(1)".

--- problems/380_insert_delete_getrandom_O(1).py
+++ problems/380_insert_delete_getrandom_O(1).py
@@ -1,25 +1,31 @@
 class RandomizedSet:
     def __init__(self):
-        self.data = {}
+        self.index_map = {}
+        self.values = []
 
     def insert(self, val: int) -> bool:
-        if self.data.get(val, False):
+        if val in self.index_map:
             return False
         else:
-            self.data[val] = True
+            self.index_map[val] = len(self.values)
+            self.values.append(val)
             return True
     
     def remove(self, val: int) -> bool:
-        if self.data.get(val, False):
-            self.data.pop(val)
+        if val in self.index_map:
+            idx = self.index_map[val]
+            del self.index_map[val]
+            last_val = self.values.pop()
+            if idx != len(self.values):
+                self.values[idx] = last_val
+                self.index_map[self.values[idx]] = idx
             return True
         else:
             return False
     
     def getRandom(self) -> int:
-        idx = 0
-        rand_idx = random.randint(0, len(self.data)-1)
-        return list(self.data.keys())[rand_idx]
+        rand_idx = random.randint(0, len(self.values)-1)
+        return self.values[rand_idx]
 
 
 
@@ -28,4 +34,3 @@ class RandomizedSet:
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
-
