33e6779bc9d933aab78987660e426d59a2689bdc
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-07-15 17:15:42 -0500

    [Feat]: Add O(n) clean solution to "Gas Station".

--- problems/134_gas_station.py
+++ problems/134_gas_station.py
@@ -2,25 +2,11 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost):
             return -1
-
-        n_pass = 0
-        step = -1
-        total_gas = 0
-        n = len(gas)
-        start_pos = None
-
-        while (step < n):
-            step += 1
-            idx = step % n
-            total_gas += (gas[idx] - cost[idx])
-            if total_gas < 0:
-                start_pos = None
-                total_gas = 0
-                n_pass = 0
-            else:
-                if start_pos is None:
-                    start_pos = idx
-                n_pass += 1
-
-        return start_pos
+        else:
+            start_pos, total_gas = 0, 0
+            for idx in range(len(gas)):
+                total_gas += (gas[idx] - cost[idx])
+                if total_gas < 0:
+                    start_pos, total_gas = idx + 1, 0
+            return start_pos
 
