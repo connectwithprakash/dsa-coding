f941a42a06e3521ac8a7232d255e58c1951ffade
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-12-14 12:31:15 -0600

    [Feat]: Add non-pythonic solution to "Length of Last Word" problem.

--- problems/058_length_of_last_word.py
+++ problems/058_length_of_last_word.py
@@ -3,3 +3,21 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         return len(s.strip().split(' ')[-1])
 
+
+# Second Attempt: Non pythonic way
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        idx, jdx = -1, -1
+        space_before = True
+        for i in range(len(s)):
+            if s[i] == ' ':
+                space_before = True
+            else:
+                if space_before:
+                    space_before = False
+                    idx = i
+                    jdx = idx
+                else:
+                    jdx = i
+        return (jdx - idx)+1
+
