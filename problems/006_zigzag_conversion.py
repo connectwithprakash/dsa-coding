46944ff2c8f05e6fa5a8269b7f51d05e8d1123dd
Prakash Chaudhary <connectwithprakash@gmail.com>
2023-12-30 23:18:13 -0600

    [Refactor]: Add slightly faster execution time solution to "Zigzag Conversion" problem.

--- problems/006_zigzag_conversion.py
+++ problems/006_zigzag_conversion.py
@@ -27,3 +27,30 @@ class Solution:
 
         return "".join(data)
 
+
+# Second attempt: slightly faster execution time (minor change)
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+
+        data = ['' for _ in range(numRows)]
+        idx = 0
+        zigzag = False
+
+        for char in s:
+            if idx == numRows:
+                zigzag = True
+                idx -= 2
+            if idx == 0:
+                zigzag = False
+
+            if not zigzag:
+                data[idx] += char
+                idx += 1
+            elif zigzag:
+                data[idx] += char
+                idx -= 1
+
+        return "".join(data)
+
