6b4d7638b6f50c6e279e219fe30fc7c5ba4c054e
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-01-03 16:34:39 -0600

    [Feat]: Add solution by subproblems O(n^2) solution to "3Sum" problem.

--- problems/015_3_sums.py
+++ problems/015_3_sums.py
@@ -13,3 +13,31 @@ class Solution:
         
         return three_sums
 
+
+# Attempt 2: Breaking into sub problems O(n^2)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        three_sums = []
+        
+        def twoSums(nums, target):
+            hashmap = {}
+            two_sums = []
+            for idx in range(len(nums)):
+                rem_val = (target-nums[idx])
+                if nums[idx] in hashmap:
+                    two_sums.append((nums[idx], hashmap[nums[idx]]))
+                else:
+                    hashmap[rem_val] = nums[idx]
+            return two_sums
+
+        for idx in range(n):
+            two_sums = twoSums(nums[idx+1:], -nums[idx])
+            for two_sum in two_sums:
+                three_nums = [nums[idx], two_sum[0], two_sum[1]]
+                three_nums = sorted(three_nums)
+                if three_nums not in three_sums:
+                    three_sums.append(three_nums)
+        
+        return three_sums
+
