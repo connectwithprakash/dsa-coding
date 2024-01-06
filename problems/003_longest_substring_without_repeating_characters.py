216b417d8022c412013e22f250b83ac3e3f04571
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-01-05 22:05:11 -0500

    [Feat]: Add O(n^2) solution to "Longest Substring Without Repeating Characters" problem.

--- problems/003_longest_substring_without_repeating_characters.py
+++ problems/003_longest_substring_without_repeating_characters.py
@@ -46,3 +46,22 @@ class Solution:
 
         return max_len
 
+# Attempt 3: O(n^2) solution
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        idx, jdx = 0, 0
+        n = len(s)
+        longest_substring_len = 0
+        while jdx < n:
+            if s[jdx] in s[idx:jdx]:
+                while s[idx] != s[jdx]:
+                    idx+= 1
+                idx += 1
+            else:
+                jdx += 1
+                
+            if (jdx-idx) > longest_substring_len:
+                longest_substring_len = (jdx-idx)
+
+        return longest_substring_len
+
