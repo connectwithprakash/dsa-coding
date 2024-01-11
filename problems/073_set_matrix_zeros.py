29f96c429c269d82b1cf69078d8b85b9bc0e2af5
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-01-11 11:59:49 -0600

    [Feat]: Add O(1) space and O(n^2) another solution to "Set Matrix Zeros" problem.

--- problems/073_set_matrix_zeros.py
+++ problems/073_set_matrix_zeros.py
@@ -32,3 +32,46 @@ class Solution:
                     traverse(idx, jdx)
                     matrix[idx][jdx] = 0
 
+# Attempt 2: Another approach with out using character O(n^2)
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m, n = len(matrix), len(matrix[0])
+
+        first_row_zero = False
+        first_col_zero = False
+
+        # Check if the first col contains 0
+        for idx in range(m):
+            if matrix[idx][0] == 0:
+                first_col_zero = True
+                break
+        # Check if the first row contains zero
+        for jdx in range(n):
+            if matrix[0][jdx] == 0:
+                first_row_zero = True
+                break
+        # Check inner matrix for zeros
+        for idx in range(m):
+            for jdx in range(0, n):
+                if matrix[idx][jdx] == 0:
+                    # Set column of first row to zero
+                    matrix[0][jdx] = 0
+                    # Set row of first column to zero
+                    matrix[idx][0] = 0
+        
+        # Fill inner matrix (except 1st row and 1st col) with zeros
+        for idx in range(1, m):
+            for jdx in range(1, n):
+                if matrix[idx][0] == 0 or matrix[0][jdx] == 0:
+                    matrix[idx][jdx] = 0
+        # Fill the first row
+        if first_col_zero:
+            for idx in range(m):
+                matrix[idx][0] = 0
+        # Fill the first column
+        if first_row_zero:
+                matrix[0] = [0]*n
+
