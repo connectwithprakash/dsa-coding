677dc2ddb14ec86f2e34e20ffe457a7ca1957426
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-01-10 19:19:29 -0600

    [Feat]: Add rotation and flip in single loop O(n^2) solution to "Rotate Matrix" problem.

--- problems/048_rotate_matrix.py
+++ problems/048_rotate_matrix.py
@@ -1,4 +1,4 @@
-# Attempt 1: Transpose and Flip the Matrix Approach
+# Attempt 1: Transpose and Flip the Matrix Approach (slightly faster solution than the attempt 2)
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
@@ -14,4 +14,22 @@ class Solution:
         for jdx in range(n//2):
             for idx in range(m):
                 matrix[idx][jdx], matrix[idx][n-1-jdx] = matrix[idx][n-1-jdx], matrix[idx][jdx]
+
+
+# Attempt 2: Both transpose and flip in the same loop (slightly slower solution)
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m, n = len(matrix), len(matrix[0])
+        mid_point = n/2
+        for idx in range(m):
+            for jdx in range(n):
+                # Transpose the matrix
+                if jdx >= idx:
+                    matrix[idx][jdx], matrix[jdx][idx] = matrix[jdx][idx], matrix[idx][jdx]
+                # Flip the matrix
+                if jdx >= mid_point:
+                    matrix[idx][jdx], matrix[idx][n-1-jdx] = matrix[idx][n-1-jdx], matrix[idx][jdx]
 
