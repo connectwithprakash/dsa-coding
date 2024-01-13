a3bace356dccbc8330ec73450d3153bd359cb322
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-01-13 15:18:08 -0600

    [Feat]: Add O(n) solution (faster) using double hashmap to "Word Pattern" problem.

--- problems/word_pattern.py
+++ problems/word_pattern.py
@@ -17,3 +17,26 @@ class Solution:
                     return False
         return True
 
+
+# Attempt 2: Using double hashmapt to remove the computation
+# of hashmap.values() in each step - Faster solution - O(n^2)
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        word2pattern = {}
+        pattern2word = {}
+        if len(pattern) != len(words):
+            return False
+        
+        for idx in range(len(words)):
+            if not word2pattern.get(words[idx], 0):
+                if not pattern2word.get(pattern[idx], 0):
+                    word2pattern[words[idx]] = pattern[idx]
+                    pattern2word[pattern[idx]] = words[idx]
+                else:
+                    return False
+            else:
+                if word2pattern[words[idx]] != pattern[idx]:
+                    return False
+        return True
+
