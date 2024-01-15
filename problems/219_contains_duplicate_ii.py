36caa26c4b763ddd3105c923ff25e7229c21be54
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-01-15 13:10:08 -0600

    [Feat]: Add optimed O(n^2) solution to "Contains Duplicate II" problem.

--- problems/219_contains_duplicate_ii.py
+++ problems/219_contains_duplicate_ii.py
@@ -8,3 +8,28 @@ class Solution:
                     return True
         return False
 
+
+# Attempt 2: Optimized O(n^2) using hashmap
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashmap = {}
+        for idx, num in enumerate(nums):
+            if num in hashmap:
+                hashmap[num].append(idx)
+            else:
+                hashmap[num] = [idx]
+
+        found = False
+        for key in hashmap:
+            values = hashmap[key]
+            if len(values) == 1:
+                continue
+            for i in range(len(values)-1):
+                if (values[i+1] - values[i]) <= k:
+                    found = True
+                    break
+            if found:
+                break
+
+        return found
+
