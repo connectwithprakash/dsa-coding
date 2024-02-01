663f859fd9e8d626ca3f2ec52c73be2ac3f33487
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-02-01 10:44:45 -0600

    [Feat]: Add O(n) solution without loosing information of node to "Linked List Cycle" problem.

--- problems/141_linked_list_cycle.py
+++ problems/141_linked_list_cycle.py
@@ -16,3 +16,16 @@ class Solution:
             temp = temp.next
         return False
 
+
+# Attempt 2: Simple solution without loosing the previous node information
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        temp = head
+
+        while temp is not None:
+            if temp.val > 10e5:
+                return True
+            temp.val += 10e6
+            temp = temp.next
+        return False
+
