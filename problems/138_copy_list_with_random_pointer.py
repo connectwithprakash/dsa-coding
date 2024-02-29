acb7757294d5a180d0ce95fd25fdd6f7d063464a
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-02-29 11:25:01 -0600

    [Feat]: Add O(n) solution to 'Copy List with Random Pointer' problem.

---
+++ problems/138_copy_list_with_random_pointer.py
@@ -0,0 +1,44 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head is None:
+            return None
+            
+        temp = head
+        results = []
+
+        # Create a copy of the nodes
+        idx = 0
+        while temp:
+            results.append(Node(temp.val, None, temp.random))
+            temp.val = idx
+            idx += 1
+            temp = temp.next
+
+        # Link node with the next node
+        for idx in range(len(results)-1):
+            results[idx].next = results[idx+1]
+
+        # Set random link to approprite node from the new list
+        for node in results:
+            node.random = results[node.random.val] if node.random else None
+
+        results = results[0]
+
+        # Revert the value of original linkedlist
+        temp1, temp2 = head, results
+        while temp1 and temp2:
+            temp1.val = temp2.val
+            temp1 = temp1.next
+            temp2 = temp2.next
+
+        return results
+
