48c75a4948f9927e2cc3930f09d5b51bbd067056
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-02-29 08:44:11 -0600

    [Feat]: Add second attempt solution to 'Add Two Numbers' problem.

--- problems/002_add_two_numbers.py
+++ problems/002_add_two_numbers.py
@@ -31,3 +31,38 @@ class Solution:
 
         return result.next
 
+
+# Attempt 2: Add Two Numbers
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        result = ListNode()
+        head = result
+        carry = 0
+        while l1 or l2:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            
+            carry, remainder = carry//10, carry%10
+            head.next = ListNode(remainder)
+            head = head.next
+
+        while carry > 9:
+            carry, remainder = carry//10, carry%10
+            head.next = ListNode(remainder)
+            head = head.next
+
+        if carry:
+            head.next = ListNode(carry)
+            head = head.next
+
+        return result.next
+
