84c06c46da0c8dde494dfe2628d921bb990239b8
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-04-10 16:37:49 -0500

    [Feat]: Add comments to "Remove Duplicates from Sorted List II" problem.

--- leet_code/scripts/082_remove_duplicates_from_sorted_list_ii.py
+++ leet_code/scripts/082_remove_duplicates_from_sorted_list_ii.py
@@ -1,27 +1,31 @@
 # Attempt 1: O(n) efficient solution
 
-# Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Adding a dummy node before the head simplifies handling edge cases.
         head = ListNode(None, head)
-        prev = head
-        curr = head.next
+        prev = head  # Pointer to the previous node
+        curr = head.next  # Pointer to the current node
 
-        repeat = False
+        repeat = False  # Flag to track if the current node's value is a duplicate
         while curr:
+            # If the current node has a next node and they have the same value,
+            # mark it as a repeat.
             if curr.next and (curr.val == curr.next.val):
                 repeat = True
+            # If the current node's value is a repeat, remove it by updating
+            # the previous node's next pointer to skip over it.
             elif repeat:
                 prev.next = curr.next
-                repeat = False
+                repeat = False  # Reset the repeat flag
             else:
-                prev = curr
+                prev = curr  # Move the previous pointer forward
 
-            curr = curr.next
+            curr = curr.next  # Move the current pointer forward
 
-        return head.next
+        return head.next  # Return the head of the modified linked list
 
