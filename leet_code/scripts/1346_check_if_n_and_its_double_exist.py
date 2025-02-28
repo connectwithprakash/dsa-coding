053ad02cb0c3dc79c5cb5935f40a716ce6e47629
Prakash Chaudhary <connectwithprakash@gmail.com>
2025-02-28 08:21:25 -0800

    [Feat]: Add faster solution to "941. Valid Mountain Array" by implementing a set-based approach

--- leet_code/scripts/1346_check_if_n_and_its_double_exist.py
+++ leet_code/scripts/1346_check_if_n_and_its_double_exist.py
@@ -5,16 +5,30 @@ equal and ith indexed element is double of the jth element or vice versa
 because the indexes coulbe be reversed
 Idea: Simplementation approach would be to take one number and compare
 it with the others. T: O(n^2), S: O(1)
+
+Second approach would be to have a set where we can look up already seen
+numbers that we can use to check if the half or double exists. T: O(n), O()
 """
 
 
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        # Simple approach
-        n = len(arr)
-        for idx in range(n - 1):
-            for jdx in range(idx + 1, n):
-                if (arr[idx] == 2 * arr[jdx]) or (arr[jdx] == 2 * arr[idx]):
-                    return True
+        # # Simple approach
+        # n = len(arr)
+        # for idx in range(n-1):
+        #     for jdx in range(idx+1, n):
+        #         if (arr[idx] == 2*arr[jdx]) or (arr[jdx] == 2*arr[idx]):
+        #             return True
+
+        # return False
+
+        # Faster approach
+        seen_nums = set()
+        for num in arr:
+            if num * 2 in seen_nums:
+                return True
+            elif (num % 2 == 0) and ((num // 2) in seen_nums):
+                return True
+            seen_nums.add(num)
 
         return False
