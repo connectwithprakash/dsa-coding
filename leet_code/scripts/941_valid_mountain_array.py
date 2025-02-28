e06782f75bf23607af8602d98a9e889ad5b89e06
Prakash Chaudhary <connectwithprakash@gmail.com>
2025-02-28 08:53:38 -0800

    [Refactor]: Simplify peak index calculation and improve slope checks in valid mountain array solution

--- leet_code/scripts/941_valid_mountain_array.py
+++ leet_code/scripts/941_valid_mountain_array.py
@@ -9,8 +9,6 @@ Simple apporach would be to to find the peak of the mountain
 Divide the array into two halves. First check for positive
 slope at each neighboring numbers and then check of negative
 slope with other half. T: O(n), S: O(1)
-
-
 """
 
 
@@ -20,7 +18,7 @@ class Solution:
         if len(arr) < 3:
             return False
         # Find the peak
-        peak_idx = next((idx for idx in range(len(arr)) if arr[idx] == max(arr)))
+        peak_idx = arr.index(max(arr))
         # Check if we have two halves or not by checking their length
         if not len(arr[:peak_idx]) or not len(arr[peak_idx + 1 :]):
             return False
@@ -28,11 +26,11 @@ class Solution:
         # Check if the array is a valid mountation
         # Verify that the left half has increasing slope
         for idx in range(1, peak_idx + 1):
-            if (arr[idx] - arr[idx - 1]) <= 0:
+            if arr[idx] <= arr[idx - 1]:
                 return False
         # Verify that the right half has decreasing slope
         for jdx in range(peak_idx, len(arr) - 1):
-            if (arr[jdx] - arr[jdx + 1]) <= 0:
+            if arr[jdx] <= arr[jdx + 1]:
                 return False
 
         return True
