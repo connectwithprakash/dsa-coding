0d8062170802c194321bf394b740d4de469d19a0
Prakash Chaudhary <connectwithprakash@gmail.com>
2025-03-01 21:15:41 -0800

    [Refactor]: Implement O(n) solution to "1299. Replace Elements with Greatest Element on Right Side"

--- leet_code/scripts/1299_replace_elements_with_greatest_element_on_right_side.py
+++ leet_code/scripts/1299_replace_elements_with_greatest_element_on_right_side.py
@@ -7,7 +7,18 @@ right of it. T: O(n^2), S: O(1)
 
 Another approach would be to move around the numbers with idx
 and find the max of rest of the array, and then move idx until
-this max value and then repeat again. T: O(n^2), S: O(1)
+this max value and then repeat again. T: O(n^2) - O(n), S: O(1)
+
+Another even faster approach would be have keep track of the
+maximum value and at each time we find the maximum value we then
+move start finding the max value again
+
+Let's try again another approach would be to look at the output.
+The output will be a decending numbered array. So, if we go from
+last we just need to keep track that the cell to the right is
+is greater than the current cell because if the right is greater 
+than the current cell then that value is the maximum to the right
+of the rest of the array. T: O(n), S: O(1)
 """
 
 
@@ -17,12 +28,23 @@ class Solution:
         #     arr[idx] = max(arr[idx+1:])
         # arr[-1] = -1
 
-        # A little faster approach
-        max_val = arr[0]
-        for idx in range(len(arr) - 1):
-            if arr[idx] == max_val:
-                max_val = max(arr[idx + 1 :])
-            arr[idx] = max_val
-        arr[-1] = -1
+        # # A little faster approach
+        # max_val = arr[0]
+        # for idx in range(len(arr)-1):
+        #     if arr[idx] == max_val:
+        #         max_val = max(arr[idx+1:])
+        #     arr[idx] = max_val
+        # arr[-1] = -1
+
+        # Yet another approach
+        max_val = -1
+        for idx in range(len(arr) - 1, -1, -1):
+            if arr[idx] > max_val:
+                # Swap variables
+                temp_val = arr[idx]
+                arr[idx] = max_val
+                max_val = temp_val
+            else:
+                arr[idx] = max_val
 
         return arr
