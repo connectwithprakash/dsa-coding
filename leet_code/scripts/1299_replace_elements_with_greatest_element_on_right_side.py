e56f98e3211508bb452853f7ec7a2551d882d764
Prakash Chaudhary <connectwithprakash@gmail.com>
2025-03-01 20:52:54 -0800

    [Refactor]: Optimize element replacement logic in "Replace Elements with Greatest Element on Right Side"

--- leet_code/scripts/1299_replace_elements_with_greatest_element_on_right_side.py
+++ leet_code/scripts/1299_replace_elements_with_greatest_element_on_right_side.py
@@ -4,13 +4,25 @@ Output: Same array with inplace replacement
 Idea: A simple idea would be to loop through all the numbers
 and just replace the value with the max of the values to the
 right of it. T: O(n^2), S: O(1)
+
+Another approach would be to move around the numbers with idx
+and find the max of rest of the array, and then move idx until
+this max value and then repeat again. T: O(n^2), S: O(1)
 """
 
 
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
+        # for idx in range(len(arr)-1):
+        #     arr[idx] = max(arr[idx+1:])
+        # arr[-1] = -1
+
+        # A little faster approach
+        max_val = arr[0]
         for idx in range(len(arr) - 1):
-            arr[idx] = max(arr[idx + 1 :])
+            if arr[idx] == max_val:
+                max_val = max(arr[idx + 1 :])
+            arr[idx] = max_val
         arr[-1] = -1
 
         return arr
