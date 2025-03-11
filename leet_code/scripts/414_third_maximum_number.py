f613f2bddde8cd4f5f4058fe6da9d029151eac80
Prakash Chaudhary <connectwithprakash@gmail.com>
2025-03-11 09:17:12 -0700

    [Refactor]: Modify solution for "414. Third Maximum Number" to improve efficiency with O(n) time complexity and O(1) space complexity

--- leet_code/scripts/414_third_maximum_number.py
+++ leet_code/scripts/414_third_maximum_number.py
@@ -5,24 +5,30 @@ Idea: A simple approach would be to create is have a list
 of size 3, and we traverse the nums and if we see maximum than
 the heap we remove the min and put current num to the list
 We return the min of that list. T: O(n*k), S: O(1)
+
+Another approach would be to have a variable that stores the three max values.
+At each iteration we check if the new number gets the place of either first
+second or third maximum place and replace old value with the new one and then
+return the third value. T: O(n), S: O(1)
 """
 
 
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        max_nums = set()
-        for idx in range(len(nums)):
-            if len(max_nums) == 3:
-                break
-            else:
-                max_nums.add(nums[idx])
-        if len(max_nums) < 3:
-            return max(max_nums)
+        if len(nums) < 3:
+            return max(nums)
+        max_nums = [float("-inf")] * 3
+        for num in nums:
+            if (num == max_nums[0]) or (num == max_nums[1]) or (num == max_nums[2]):
+                continue
+            elif num > max_nums[0]:
+                max_nums = [num, max_nums[0], max_nums[1]]
+            elif num > max_nums[1]:
+                max_nums[1:] = [num, max_nums[1]]
+            elif num > max_nums[2]:
+                max_nums[2] = num
+
+        if max_nums[2] != float("-inf"):
+            return max_nums[2]
         else:
-            start_idx = idx
-            while start_idx < len(nums):  # O(n)
-                if nums[start_idx] not in max_nums and nums[start_idx] > min(max_nums):  # O(k)
-                    max_nums.remove(min(max_nums))  # O(2*k)
-                    max_nums.add(nums[start_idx])  # O(k)
-                start_idx += 1
-            return min(max_nums)
+            return max_nums[0]
