d35c13c682259b1b52353c00686cbaf35d0fe989
Prakash Chaudhary <connectwithprakash@gmail.com>
2025-04-02 20:59:49 -0700

    [Refactor]: Update longest common prefix solution to use a more efficient approach by reducing the prefix based on string comparisons

--- leet_code/scripts/014_longest_common_prefix.py
+++ leet_code/scripts/014_longest_common_prefix.py
@@ -1,13 +1,25 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        min_len = min(len(string) for string in strs)
-        common_prefix = ""
-        for i in range(min_len):
-            char = strs[0][i]
-            for string in strs:
-                if char != string[i]:
-                    return common_prefix
-            common_prefix += char
-        
-        return common_prefix
+        # common_prefix = ""
+        #         for i in range(min_len):
+        #             char = strs[0][i]
+        #             for string in strs:
+        #                 if char != string[i]:
+        #                     return common_prefix
+        #             common_prefix += char
 
+        #         return common_prefix
+
+        # Another approach
+        # Start with the first string as the initial prefix
+        common_prefix = strs[0]
+
+        # Compare the prefix with each string in the list
+        for s in strs[1:]:
+            # Reduce the prefix until it matches the start of the current string
+            while not s.startswith(common_prefix):
+                common_prefix = common_prefix[:-1]
+                if not common_prefix:
+                    return ""
+
+        return common_prefix
