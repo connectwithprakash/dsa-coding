f58028b4560ed579ea672e48eed041172342e296
Prakash Chaudhary <connectwithprakash@gmail.com>
2025-04-09 20:12:26 -0700

    [Refactor]: Optimize array rotation logic by implementing a reverse-based approach for improved clarity and efficiency

--- leet_code/scripts/189_rotate_array.py
+++ leet_code/scripts/189_rotate_array.py
@@ -1,31 +1,33 @@
-## Attempt 1: Pythonic solution
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Naive pythonic way
-        k = (k % len(nums))
-        nums[:] = nums[-k:] + nums[:-k]
+        # offset = 0
+        # n = len(nums)
+        # while True:
+        #     k = k % (n - offset)
+        #     if k == 0:
+        #         break
+        #     for i in range(n-k, n):
+        #         swap_i = ((i + k) % n) + offset
+        #         nums[swap_i], nums[i] = nums[i], nums[swap_i]
+        #         print(nums, k)
+            # offset += k
 
-
-## Approach 2: Divide the array into from pivot idx and reverse each and then reverse combined.
-
-
-## Attempt 2: My solution
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        offset = 0
+        # Another easier to understand solution
         n = len(nums)
-        while True:
-            k = k % (n - offset)
-            if k == 0:
-                break
-            for i in range(n-k, n):
-                swap_i = ((i + k) % n) + offset
-                nums[swap_i], nums[i] = nums[i], nums[swap_i]
-            offset += k
+        k = k % n
+        def reverse(nums, left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        
+        # Reverse full array
+        reverse(nums, 0, n-1)
+        # Reverse left half
+        reverse(nums, 0, k-1)
+        # Reverse right half
+        reverse(nums, k, n-1)
 
